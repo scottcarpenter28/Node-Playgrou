dc4b408bf913c71474fe03c9ba238e38350ebfba
Scott Carpenter <scottcarpenter28@gmail.com>
2023-10-28 15:44:19 -0400

    Start adding nextjs

--- ENV/app.js
+++ ENV/app.js
@@ -1,1 +1,2 @@
+// Start the app with  node --env-file=.env app.js
 console.log(process.env.EXAMPLE)
---
+++ React/learn_next_js_react/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <h1>Hello from react</h1>
+    </main>
+  )
+}
